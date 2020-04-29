2400e273d2af07d853d2d73939dcfc3e8a539bf5
Ibrahim Shaaban <hemaabdelnaby325@gmail.com>
2020-04-29 22:33:08 +0200

    finish scrapping from okaz

--- src/shared/scrapper/mapping-categories.ts
+++ src/shared/scrapper/mapping-categories.ts
@@ -21,15 +21,3 @@ okazMapper.Games = 'Games';
 okazMapper['special-zones'] = 'society';
 
 export const okaz = okazMapper;
-//  "news": 'local', // old
-//  "sports": 'sports', // old
-//  "economy": 'economy', // old
-//  "fashion": 'fashion', // new
-//  "tourism": 'tourism', // old
-//  'people-situations': 'local', // old
-//  investigation: 'society', // old
-//  technology: 'technology', // new
-//  'citizen-voice': 'local', // old
-//  variety: 'misc', // old
-//  Games: 'Games', // new
-//  'special-zones': 'society', // old
